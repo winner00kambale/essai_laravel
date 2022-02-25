5c958f7579ddfbf9ca4751398517e67998bc7ff7
winner00kambale <vainqueurkambale@gmail.com>
2022-02-25 10:49:36 +0200

    ajax

--- app/Http/Controllers/EtudiantController.php
+++ app/Http/Controllers/EtudiantController.php
@@ -20,7 +20,9 @@ class EtudiantController extends Controller
             'age' => $request->age,
         ]);
         //return back()->with('message','insertion avec succes');
-        return redirect()->route('aficher_etudiant')->with('message','insertion avec succes');
+        // return redirect()->route('aficher_etudiant')->with('message','insertion avec succes');
+        return response()->json(['message' => 'inserted succes']);
+
     }
     public function edit($id){
         $data = \DB::select("SELECT * FROM etudiants WHERE id = ?",[$id]);
@@ -29,6 +31,7 @@ class EtudiantController extends Controller
     }
     public function update(Request $request){
         \DB::update('UPDATE etudiants set noms = ?,age = ? where id = ?',[$request->noms,$request->age,$request->id]);
+        return redirect()->route('aficher_etudiant');
     }
     public function destroy($id){
         \DB::delete('DELETE FROM etudiants where id = ?',[$id]);
--- resources/views/new_etudiants.blade.php
+++ resources/views/new_etudiants.blade.php
@@ -9,11 +9,36 @@
         @endforeach
         </div>
     @endif
-    <form method="post" action="{{ route('store-etudiant') }}">
+    <form id="monformulaire">
         @csrf
-        <input type="text" name="noms" placeholder="saisir votre  nom">
-        <input type="number" name="age" min="0" step="1" placeholder="saisir l'age">
+        <input type="text" name="noms" id="noms" placeholder="saisir votre  nom">
+        <input type="number" name="age" id="age" min="0" step="1" placeholder="saisir l'age">
         <input type="submit" value="enregistrer">
     </form>
 </div>
+@endsection
+
+@section('monscript')
+    <script>
+        $('#monformulaire').submit(function(event){
+            event.preventDefault();
+
+            $.ajax({
+                url : '{{ route("store-etudiant") }}',
+                method : 'POST',
+                data : new FormData(this),
+                processData : false,
+                contentType : false,
+                cache : false,
+                headers : {'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')},
+                
+                success : function(data){
+                    alert('Inserted succes');
+                    $('#monformulaire')[0].reset();
+                }
+                
+
+            });
+        });
+    </script>
 @endsection
